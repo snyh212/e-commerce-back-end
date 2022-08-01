b9f050db5b736c83cea583227e14504e208a0cbd
Shmuel Hoffman <snyh212@gmail.com>
2022-07-31 23:18:21 -0400

    added sequelize connection and synced with port creation

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('sequelize');
+const mysql = require ('mysql2');
 // import sequelize connection
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "ecommerce_db"
+});
+connection.connect(function(err){
+  if(err) throw err;
+  console.log("MYSQL connected!");
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync().then(() =>{
+  app.listen(PORT, () => {
+    console.log(`App listening on port http://localhost:${PORT}`);
+  });
 });
