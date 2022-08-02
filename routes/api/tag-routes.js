2cf17fe3df16a035caad20562c4ccb2d7d504280
Shmuel Hoffman <snyh212@gmail.com>
2022-08-02 00:47:35 -0400

    added last routes and changed catagories-name to category_name

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Category.create(req.body)
@@ -43,8 +43,18 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    }) 
+      res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
