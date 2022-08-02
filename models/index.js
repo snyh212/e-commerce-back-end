4697bcb2f696874bc089c595ea25892c768870df
Shmuel Hoffman <snyh212@gmail.com>
2022-08-01 21:07:02 -0400

    connected table models in index.js with belong/has to

--- models/index.js
+++ models/index.js
@@ -9,11 +9,19 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 // Categories have many Products
-Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-Product
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
-Tag
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
